21a8a519bebe262c63ea93ad9a167531eef22fdd
Felipe Lima <lms.felipe@gmail.com>
2024-09-10 19:12:30 -0300

    fix: rename find patient user case

--- src/use-cases/patient/findPatients.ts
+++ src/use-cases/patient/findPatients.ts
@@ -1,14 +1,14 @@
 import { IPatient } from "../../entities/Patient";
 import { IPatientRepository } from "../../repositories/patientRepository";
 
-export class FindPatient {
+export class FindPatients {
   patientRepository: IPatientRepository;
 
   constructor(patientRepository: IPatientRepository) {
     this.patientRepository = patientRepository;
   }
 
-  async execute(id: number): Promise<IPatient | null> {
-    return await this.patientRepository.findOne(id);
+  async execute(ownerId: number): Promise<IPatient[] | null> {
+    return await this.patientRepository.findAll(ownerId);
   }
 }
